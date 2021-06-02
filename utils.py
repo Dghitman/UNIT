cbacc0695deefe6169644a7d599908c5aaa98138
Daniil Gurev <70944657+Dghitman@users.noreply.github.com>
2021-06-02 18:56:31 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@
 Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
-import torchfile
 from torch.utils.data import DataLoader
 from networks import Vgg16
 from torch.autograd import Variable
